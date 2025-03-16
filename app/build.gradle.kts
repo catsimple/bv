e8e243c1af90e2d29c15c67e11fa33170ac145b2
catsimple <catsimple@qq.com>
2025-03-16 22:31:02 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,6 @@ android {
                 storePassword = properties.getProperty("keystore.pwd")
                 keyAlias = properties.getProperty("keystore.alias")
                 keyPassword = properties.getProperty("keystore.alias_pwd")
-                v1SigningEnabled = true
-                v2SigningEnabled = true
-                v3SigningEnabled = true
             }
         }
     }
