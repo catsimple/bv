7afddcae5c4536b8d5a25a72d1f7b62d00dfbd5c
catsimple <catsimple@qq.com>
2025-03-22 22:37:09 +0800

    Update PgcContent.kt

--- app/src/main/kotlin/dev/aaa1115910/bv/screen/main/PgcContent.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/screen/main/PgcContent.kt
@@ -1,178 +1,200 @@
-package dev.aaa1115910.bv.screen.main
+package dev.aaa1115910.bv.viewmodel.pgc
 
-import androidx.activity.compose.BackHandler
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.togetherWith
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.unit.dp
-import dev.aaa1115910.bv.component.PgcTopNavItem
-import dev.aaa1115910.bv.component.TopNav
-import dev.aaa1115910.bv.screen.main.pgc.AnimeContent
-import dev.aaa1115910.bv.screen.main.pgc.DocumentaryContent
-import dev.aaa1115910.bv.screen.main.pgc.GuoChuangContent
-import dev.aaa1115910.bv.screen.main.pgc.MovieContent
-import dev.aaa1115910.bv.screen.main.pgc.TvContent
-import dev.aaa1115910.bv.screen.main.pgc.VarietyContent
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.aaa1115910.biliapi.entity.CarouselData
+import dev.aaa1115910.biliapi.entity.pgc.PgcFeedData
+import dev.aaa1115910.biliapi.entity.pgc.PgcItem
+import dev.aaa1115910.biliapi.entity.pgc.PgcType
+import dev.aaa1115910.biliapi.repositories.PgcRepository
+import dev.aaa1115910.bv.BVApp
+import dev.aaa1115910.bv.BuildConfig
+import dev.aaa1115910.bv.util.addWithMainContext
+import dev.aaa1115910.bv.util.addAllWithMainContext
 import dev.aaa1115910.bv.util.fInfo
-import dev.aaa1115910.bv.util.requestFocus
-import dev.aaa1115910.bv.viewmodel.pgc.PgcAnimeViewModel
-import dev.aaa1115910.bv.viewmodel.pgc.PgcDocumentaryViewModel
-import dev.aaa1115910.bv.viewmodel.pgc.PgcGuoChuangViewModel
-import dev.aaa1115910.bv.viewmodel.pgc.PgcMovieViewModel
-import dev.aaa1115910.bv.viewmodel.pgc.PgcTvViewModel
-import dev.aaa1115910.bv.viewmodel.pgc.PgcVarietyViewModel
+import dev.aaa1115910.bv.util.fWarn
+import dev.aaa1115910.bv.util.toast
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import org.koin.androidx.compose.koinViewModel
+import kotlinx.coroutines.withContext
 
-@Composable
-fun PgcContent(
-    modifier: Modifier = Modifier,
-    navFocusRequester: FocusRequester,
-    pgcAnimeViewModel: PgcAnimeViewModel = koinViewModel(),
-    pgcGuoChuangViewModel: PgcGuoChuangViewModel = koinViewModel(),
-    pgcMovieViewModel: PgcMovieViewModel = koinViewModel(),
-    pgcDocumentaryViewModel: PgcDocumentaryViewModel = koinViewModel(),
-    pgcTvViewModel: PgcTvViewModel = koinViewModel(),
-    pgcVarietyViewModel: PgcVarietyViewModel = koinViewModel()
-) {
-    val scope = rememberCoroutineScope()
-    val logger = KotlinLogging.logger("PgcContent")
+abstract class PgcViewModel(
+    open val pgcRepository: PgcRepository,
+    val pgcType: PgcType,
+) : ViewModel() {
+    private val logger = KotlinLogging.logger("PgcViewModel[$pgcType]")
 
-    val animeState = rememberLazyListState()
-    val guoChuangState = rememberLazyListState()
-    val movieState = rememberLazyListState()
-    val documentaryState = rememberLazyListState()
-    val tvState = rememberLazyListState()
-    val varietyState = rememberLazyListState()
+    /**
+     * 轮播图
+     */
+    val carouselItems = mutableStateListOf<CarouselData.CarouselItem>()
 
-    var selectedTab by remember { mutableStateOf(PgcTopNavItem.Anime) }
-    var focusOnContent by remember { mutableStateOf(false) }
-    val currentListOnTop by remember {
-        derivedStateOf {
-            with(
-                when (selectedTab) {
-                    PgcTopNavItem.Anime -> animeState
-                    PgcTopNavItem.GuoChuang -> guoChuangState
-                    PgcTopNavItem.Movie -> movieState
-                    PgcTopNavItem.Documentary -> documentaryState
-                    PgcTopNavItem.Tv -> tvState
-                    PgcTopNavItem.Variety -> varietyState
-                }
-            ) {
-                firstVisibleItemIndex == 0 && firstVisibleItemScrollOffset == 0
+    /**
+     * 猜你喜欢
+     */
+    val feedItems = mutableStateListOf<FeedListItem>()
+
+    /**
+     * 推荐数据中会穿插排行榜，为了避免出现某一行仅出现单独几个剧集，因此将不满一行的剧集单独存起来
+     */
+    private val restSubItems = mutableListOf<PgcItem>()
+
+    var updating by mutableStateOf(false)
+    var hasNext by mutableStateOf(true)
+    private var cursor = 0
+
+    init {
+        loadMore()
+        viewModelScope.launch(Dispatchers.IO) {
+            updateCarousel()
+        }
+    }
+
+    /**
+     * 加载更多推荐数据
+     */
+    fun loadMore() {
+        if (hasNext) {
+            viewModelScope.launch(Dispatchers.IO) {
+                updateFeed()
             }
         }
     }
 
-    //启动时刷新数据
-    LaunchedEffect(Unit) {
-
-    }
-
-    // 添加监听标签变化的LaunchedEffect
-    LaunchedEffect(selectedTab) {
-        when (selectedTab) {
-            PgcTopNavItem.Anime -> pgcAnimeViewModel.getFeedIfEmpty()
-            PgcTopNavItem.GuoChuang -> pgcGuoChuangViewModel.getFeedIfEmpty()
-            PgcTopNavItem.Movie -> pgcMovieViewModel.getFeedIfEmpty()
-            PgcTopNavItem.Documentary -> pgcDocumentaryViewModel.getFeedIfEmpty()
-            PgcTopNavItem.Tv -> pgcTvViewModel.getFeedIfEmpty()
-            PgcTopNavItem.Variety -> pgcVarietyViewModel.getFeedIfEmpty()
+    /**
+     * 重新加载所有数据，点击界面顶部 Tab 时使用
+     */
+    fun reloadAll() {
+        logger.fInfo { "Reload all $pgcType data" }
+        clearAll()
+        viewModelScope.launch(Dispatchers.IO) {
+            updateCarousel()
+            updateFeed()
         }
-        // 内容加载完成后强制返回导航栏焦点
-        navFocusRequester.requestFocus(scope)
     }
 
-    BackHandler(focusOnContent) {
-        logger.fInfo { "onFocusBackToNav" }
-        navFocusRequester.requestFocus(scope)
-        // scroll to top
-        scope.launch(Dispatchers.Main) {
-            when (selectedTab) {
-                PgcTopNavItem.Anime -> animeState.animateScrollToItem(0)
-                PgcTopNavItem.GuoChuang -> guoChuangState.animateScrollToItem(0)
-                PgcTopNavItem.Movie -> movieState.animateScrollToItem(0)
-                PgcTopNavItem.Documentary -> documentaryState.animateScrollToItem(0)
-                PgcTopNavItem.Tv -> tvState.animateScrollToItem(0)
-                PgcTopNavItem.Variety -> varietyState.animateScrollToItem(0)
+    /**
+     * 清理所有数据
+     */
+    fun clearAll() {
+        logger.fInfo { "Clear all data" }
+        carouselItems.clear()
+        feedItems.clear()
+        restSubItems.clear()
+        cursor = 0
+        hasNext = true
+    }
+
+    /**
+     * 如果数据为空则获取数据
+     */
+    fun getFeedIfEmpty() {
+        if (feedItems.isEmpty()) {
+            viewModelScope.launch(Dispatchers.IO) {
+                updateFeed()
+                updateCarousel()
             }
         }
     }
 
-    Scaffold(
-        modifier = Modifier,
-        topBar = {
-            TopNav(
-                modifier = Modifier
-                    .focusRequester(navFocusRequester)
-                    .padding(end = 80.dp),
-                items = PgcTopNavItem.entries,
-                isLargePadding = !focusOnContent && currentListOnTop,
-                onSelectedChanged = { nav ->
-                    selectedTab = nav as PgcTopNavItem
-                },
-                onClick = { nav ->
-                    when (nav) {
-                        PgcTopNavItem.Anime -> pgcAnimeViewModel.reloadAll()
-                        PgcTopNavItem.GuoChuang -> pgcGuoChuangViewModel.reloadAll()
-                        PgcTopNavItem.Movie -> pgcMovieViewModel.reloadAll()
-                        PgcTopNavItem.Documentary -> pgcDocumentaryViewModel.reloadAll()
-                        PgcTopNavItem.Tv -> pgcTvViewModel.reloadAll()
-                        PgcTopNavItem.Variety -> pgcVarietyViewModel.reloadAll()
-                    }
+    /**
+     * 更新轮播图
+     */
+    private suspend fun updateCarousel() {
+        logger.fInfo { "Updating $pgcType carousel" }
+        runCatching {
+            // 由于未知原因，注入的 PgcRepository 可能获取到的对象为 null
+            var maxRetry = 10
+            while (pgcRepository == null && maxRetry > 0) {
+                delay(10)
+                maxRetry--
+            }
+            if (BuildConfig.DEBUG && maxRetry != 10) {
+                logger.fWarn { "Retry ${10 - maxRetry} times to get pgcRepository" }
+                withContext(Dispatchers.Main) {
+                    "Retry ${10 - maxRetry} times to get pgcRepository($pgcType)".toast(BVApp.context)
                 }
+            }
+
+            val carouselData = pgcRepository.getCarousel(pgcType)
+            logger.fInfo { "Find $pgcType carousels, size: ${carouselData.items.size}" }
+            carouselItems.addAllWithMainContext(carouselData.items)
+            logger.debug { "carouselItems: $carouselItems" }
+        }.onFailure {
+            logger.fInfo { "Update $pgcType carousel failed: ${it.stackTraceToString()}" }
+            withContext(Dispatchers.Main) {
+                "加载 $pgcType 轮播图失败: ${it.message}".toast(BVApp.context)
+            }
+        }
+    }
+
+    /**
+     * 获取推荐数据
+     */
+    private suspend fun updateFeed() {
+        if (updating) return
+        withContext(Dispatchers.Main) { updating = true }
+        logger.fInfo { "Update anime feed" }
+        runCatching {
+            val pgcFeedData = pgcRepository.getFeed(
+                pgcType = pgcType,
+                cursor = cursor
+            )
+            cursor = pgcFeedData.cursor
+            withContext(Dispatchers.Main) { hasNext = pgcFeedData.hasNext }
+            updateFeedItems(pgcFeedData)
+        }.onFailure {
+            logger.fInfo { "Update $pgcType feeds failed: ${it.stackTraceToString()}" }
+        }
+        withContext(Dispatchers.Main) { updating = false }
+    }
+
+    /**
+     * 对 [updateFeed] 获取到得数据进行二次整理并更新到 feedItems
+     */
+    private suspend fun updateFeedItems(data: PgcFeedData) {
+        logger.fInfo { "update $pgcType feed items: [items: ${data.items.size}, ranks: ${data.ranks.size}]" }
+        val epList = mutableStateListOf<PgcItem>()
+        epList.addAll(restSubItems)
+        epList.addAll(data.items)
+
+        epList.chunked(5).forEach { chunkedVCardList ->
+            if (chunkedVCardList.size == 5) {
+                feedItems.addWithMainContext(
+                    FeedListItem(
+                        type = FeedListType.Ep,
+                        items = chunkedVCardList
+                    )
+                )
+            } else {
+                restSubItems.clear()
+                restSubItems.addAll(chunkedVCardList)
+            }
+        }
+
+        data.ranks.forEach { rank ->
+            feedItems.addWithMainContext(
+                FeedListItem(
+                    type = FeedListType.Rank,
+                    rank = rank
+                )
             )
         }
-    ) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .padding(innerPadding)
-                .onFocusChanged { focusOnContent = it.hasFocus }
-        ) {
-            AnimatedContent(
-                targetState = selectedTab,
-                label = "pgc animated content",
-                transitionSpec = {
-                    val coefficient = 10
-                    if (targetState.ordinal < initialState.ordinal) {
-                        fadeIn() + slideInHorizontally { -it / coefficient } togetherWith
-                                fadeOut() + slideOutHorizontally { it / coefficient }
-                    } else {
-                        fadeIn() + slideInHorizontally { it / coefficient } togetherWith
-                                fadeOut() + slideOutHorizontally { -it / coefficient }
-                    }
-                }
-            ) { screen ->
-                when (screen) {
-                    PgcTopNavItem.Anime -> AnimeContent(lazyListState = animeState)
-                    PgcTopNavItem.GuoChuang -> GuoChuangContent(lazyListState = guoChuangState)
-                    PgcTopNavItem.Movie -> MovieContent(lazyListState = movieState)
-                    PgcTopNavItem.Documentary -> DocumentaryContent(lazyListState = documentaryState)
-                    PgcTopNavItem.Tv -> TvContent(lazyListState = tvState)
-                    PgcTopNavItem.Variety -> VarietyContent(lazyListState = varietyState)
-                }
-            }
-        }
     }
+}
+
+data class FeedListItem(
+    val type: FeedListType,
+    val items: List<PgcItem>? = emptyList(),
+    val rank: PgcFeedData.FeedRank? = null
+)
+
+enum class FeedListType {
+    Ep, Rank
 }
