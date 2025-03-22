4625e58b74ee3f56148c0eeac406a62fa90ef7ca
catsimple <catsimple@qq.com>
2025-03-23 01:39:53 +0800

    Update UgcContent.kt

--- app/src/main/kotlin/dev/aaa1115910/bv/screen/main/UgcContent.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/screen/main/UgcContent.kt
@@ -1,278 +1,183 @@
-package dev.aaa1115910.bv.screen.main.ugc
+package dev.aaa1115910.bv.screen.main
 
-import android.content.Context
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import dev.aaa1115910.biliapi.entity.CarouselData
-import dev.aaa1115910.biliapi.entity.ugc.UgcItem
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import dev.aaa1115910.biliapi.entity.ugc.UgcType
-import dev.aaa1115910.biliapi.entity.ugc.region.UgcRegionPage
-import dev.aaa1115910.biliapi.repositories.UgcRepository
-import dev.aaa1115910.bv.activities.video.VideoInfoActivity
-import dev.aaa1115910.bv.component.UgcCarousel
-import dev.aaa1115910.bv.component.videocard.SmallVideoCard
-import dev.aaa1115910.bv.entity.carddata.VideoCardData
+import dev.aaa1115910.bv.component.TopNav
+import dev.aaa1115910.bv.component.UgcTopNavItem
+import dev.aaa1115910.bv.screen.main.ugc.AnimalContent
+import dev.aaa1115910.bv.screen.main.ugc.CarContent
+import dev.aaa1115910.bv.screen.main.ugc.CinephileContent
+import dev.aaa1115910.bv.screen.main.ugc.DanceContent
+import dev.aaa1115910.bv.screen.main.ugc.DougaContent
+import dev.aaa1115910.bv.screen.main.ugc.EntContent
+import dev.aaa1115910.bv.screen.main.ugc.FashionContent
+import dev.aaa1115910.bv.screen.main.ugc.FoodContent
+import dev.aaa1115910.bv.screen.main.ugc.GameContent
+import dev.aaa1115910.bv.screen.main.ugc.InformationContent
+import dev.aaa1115910.bv.screen.main.ugc.KichikuContent
+import dev.aaa1115910.bv.screen.main.ugc.KnowledgeContent
+import dev.aaa1115910.bv.screen.main.ugc.LifeContent
+import dev.aaa1115910.bv.screen.main.ugc.MusicContent
+import dev.aaa1115910.bv.screen.main.ugc.SportsContent
+import dev.aaa1115910.bv.screen.main.ugc.TechContent
+import dev.aaa1115910.bv.screen.main.ugc.UgcScaffoldState
+import dev.aaa1115910.bv.screen.main.ugc.rememberUgcScaffoldState
 import dev.aaa1115910.bv.util.fInfo
-import dev.aaa1115910.bv.util.toast
+import dev.aaa1115910.bv.util.requestFocus
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.koin.compose.koinInject
 
 @Composable
-fun UgcRegionScaffold(
+fun UgcContent(
     modifier: Modifier = Modifier,
-    state: UgcScaffoldState,
-    childRegionButtons: (@Composable () -> Unit)? = null
+    navFocusRequester: FocusRequester,
+    dougaState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Douga),
+    gameState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Game),
+    kichikuState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Kichiku),
+    musicState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Music),
+    danceState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Dance),
+    cinephileState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Cinephile),
+    entState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Ent),
+    knowledgeState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Knowledge),
+    techState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Tech),
+    informationState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Information),
+    foodState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Food),
+    lifeState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Life),
+    carState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Car),
+    fashionState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Fashion),
+    sportsState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Sports),
+    animalState: UgcScaffoldState = rememberUgcScaffoldState(ugcType = UgcType.Animal)
 ) {
-    val context = LocalContext.current
-    var currentFocusedIndex by remember { mutableIntStateOf(0) }
-    val shouldLoadMore by remember {
-        derivedStateOf { currentFocusedIndex + 24 > state.ugcItems.size }
+    val scope = rememberCoroutineScope()
+    val logger = KotlinLogging.logger("UgcContent")
+
+    var selectedTab by remember { mutableStateOf(UgcTopNavItem.Douga) }
+    var focusOnContent by remember { mutableStateOf(false) }
+
+    //启动时刷新数据
+    LaunchedEffect(Unit) {
+
     }
 
-    LaunchedEffect(Unit) { if (state.ugcItems.isEmpty()) state.initUgcRegionData() }
-    LaunchedEffect(shouldLoadMore) {
-        if (shouldLoadMore) {
-            state.loadMore()
-            currentFocusedIndex = -100
+    BackHandler(focusOnContent) {
+        logger.fInfo { "onFocusBackToNav" }
+        navFocusRequester.requestFocus(scope)
+        // scroll to top
+        scope.launch(Dispatchers.Main) {
+            when (selectedTab) {
+                UgcTopNavItem.Douga -> dougaState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Game -> gameState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Kichiku -> kichikuState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Music -> musicState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Dance -> danceState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Cinephile -> cinephileState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Ent -> entState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Knowledge -> knowledgeState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Tech -> techState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Information -> informationState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Food -> foodState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Life -> lifeState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Car -> carState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Fashion -> fashionState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Sports -> sportsState.lazyListState.animateScrollToItem(0)
+                UgcTopNavItem.Animal -> animalState.lazyListState.animateScrollToItem(0)
+            }
         }
     }
 
-    LazyColumn(
+    Scaffold(
         modifier = modifier,
-        state = state.lazyListState
-    ) {
-        if (state.showCarousel) {
-            item {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .horizontalScroll(rememberScrollState()),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    UgcCarousel(
-                        modifier = Modifier
-                            .width(880.dp)
-                            .padding(32.dp, 0.dp)
-                            .onFocusChanged { 
-                                if (it.hasFocus) {
-                                    currentFocusedIndex = -1
-                                }
-                            },
-                        data = state.carouselItems
-                    )
-                }
-            }
-        }
-
-        if (childRegionButtons != null) {
-            item {
-                childRegionButtons()
-            }
-        } else {
-            item {
-                Spacer(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(12.dp)
-                )
-            }
-        }
-
-        gridItems(
-            data = state.ugcItems,
-            columnCount = 4,
-            modifier = Modifier
-                .width(880.dp)
-                .padding(horizontal = 24.dp, vertical = 12.dp),
-            horizontalArrangement = Arrangement.spacedBy(24.dp),
-            itemContent = { index, item ->
-                SmallVideoCard(
-                    data = VideoCardData(
-                        avid = item.aid,
-                        title = item.title,
-                        cover = item.cover,
-                        play = item.play,
-                        danmaku = item.danmaku,
-                        upName = item.author,
-                        time = item.duration * 1000L
-                    ),
-                    onClick = { VideoInfoActivity.actionStart(context, item.aid) },
-                    onFocus = { currentFocusedIndex = index }
-                )
-            }
-        )
-    }
-}
-
-fun <T> LazyListScope.gridItems(
-    data: List<T>,
-    key: ((index: Int) -> Any)? = null,
-    columnCount: Int,
-    modifier: Modifier = Modifier,
-    verticalAlignment: Alignment.Vertical = Alignment.Top,
-    horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
-    itemContent: @Composable BoxScope.(Int, T) -> Unit,
-) {
-    val size = data.count()
-    val rows = if (size == 0) 0 else 1 + (size - 1) / columnCount
-    items(rows, key = key) { rowIndex ->
-        LazyRow(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            item {
-                Row(
-                    verticalAlignment = verticalAlignment,
-                    horizontalArrangement = horizontalArrangement,
-                    modifier = modifier
-                ) {
-                    for (columnIndex in 0 until columnCount) {
-                        val itemIndex = rowIndex * columnCount + columnIndex
-                        if (itemIndex < size) {
-                            Box(
-                                modifier = Modifier.weight(1F, fill = true),
-                                propagateMinConstraints = true
-                            ) {
-                                itemContent(itemIndex, data[itemIndex])
-                            }
-                        } else {
-                            Spacer(Modifier.weight(1F, fill = true))
-                        }
+        topBar = {
+            TopNav(
+                modifier = Modifier
+                    .focusRequester(navFocusRequester),
+                items = UgcTopNavItem.entries,
+                isLargePadding = !focusOnContent,
+                onSelectedChanged = { nav ->
+                    selectedTab = nav as UgcTopNavItem
+                },
+                onClick = { nav ->
+                    when (nav) {
+                        UgcTopNavItem.Douga -> dougaState.reloadAll()
+                        UgcTopNavItem.Game -> gameState.reloadAll()
+                        UgcTopNavItem.Kichiku -> kichikuState.reloadAll()
+                        UgcTopNavItem.Music -> musicState.reloadAll()
+                        UgcTopNavItem.Dance -> danceState.reloadAll()
+                        UgcTopNavItem.Cinephile -> cinephileState.reloadAll()
+                        UgcTopNavItem.Ent -> entState.reloadAll()
+                        UgcTopNavItem.Knowledge -> knowledgeState.reloadAll()
+                        UgcTopNavItem.Tech -> techState.reloadAll()
+                        UgcTopNavItem.Information -> informationState.reloadAll()
+                        UgcTopNavItem.Food -> foodState.reloadAll()
+                        UgcTopNavItem.Life -> lifeState.reloadAll()
+                        UgcTopNavItem.Car -> carState.reloadAll()
+                        UgcTopNavItem.Fashion -> fashionState.reloadAll()
+                        UgcTopNavItem.Sports -> sportsState.reloadAll()
+                        UgcTopNavItem.Animal -> animalState.reloadAll()
                     }
                 }
+            )
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .onFocusChanged { focusOnContent = it.hasFocus }
+        ) {
+            AnimatedContent(
+                targetState = selectedTab,
+                label = "ugc animated content",
+                transitionSpec = {
+                    val coefficient = 10
+                    if (targetState.ordinal < initialState.ordinal) {
+                        fadeIn() + slideInHorizontally { -it / coefficient } togetherWith
+                                fadeOut() + slideOutHorizontally { it / coefficient }
+                    } else {
+                        fadeIn() + slideInHorizontally { it / coefficient } togetherWith
+                                fadeOut() + slideOutHorizontally { -it / coefficient }
+                    }
+                }
+            ) { screen ->
+                when (screen) {
+                    UgcTopNavItem.Douga -> DougaContent(state = dougaState)
+                    UgcTopNavItem.Game -> GameContent(state = gameState)
+                    UgcTopNavItem.Kichiku -> KichikuContent(state = kichikuState)
+                    UgcTopNavItem.Music -> MusicContent(state = musicState)
+                    UgcTopNavItem.Dance -> DanceContent(state = danceState)
+                    UgcTopNavItem.Cinephile -> CinephileContent(state = cinephileState)
+                    UgcTopNavItem.Ent -> EntContent(state = entState)
+                    UgcTopNavItem.Knowledge -> KnowledgeContent(state = knowledgeState)
+                    UgcTopNavItem.Tech -> TechContent(state = techState)
+                    UgcTopNavItem.Information -> InformationContent(state = informationState)
+                    UgcTopNavItem.Food -> FoodContent(state = foodState)
+                    UgcTopNavItem.Life -> LifeContent(state = lifeState)
+                    UgcTopNavItem.Car -> CarContent(state = carState)
+                    UgcTopNavItem.Fashion -> FashionContent(state = fashionState)
+                    UgcTopNavItem.Sports -> SportsContent(state = sportsState)
+                    UgcTopNavItem.Animal -> AnimalContent(state = animalState)
+                }
             }
         }
-    }
-}
-
-data class UgcScaffoldState(
-    val context: Context,
-    val scope: CoroutineScope,
-    val lazyListState: LazyListState,
-    val ugcType: UgcType,
-    private val ugcRepository: UgcRepository
-) {
-    companion object {
-        val logger = KotlinLogging.logger { }
-    }
-
-    val carouselItems = mutableStateListOf<CarouselData.CarouselItem>()
-    val ugcItems = mutableStateListOf<UgcItem>()
-    var nextPage by mutableStateOf(UgcRegionPage())
-    var hasMore by mutableStateOf(true)
-    var updating by mutableStateOf(false)
-    var showCarousel by mutableStateOf(true)
-
-    suspend fun initUgcRegionData() {
-        loadUgcRegionData()
-        loadMore()
-    }
-
-    suspend fun loadUgcRegionData() {
-        if (!hasMore && updating) return
-        updating = true
-        logger.fInfo { "load ugc $ugcType region data" }
-        runCatching {
-            val data = ugcRepository.getRegionData(ugcType)
-            carouselItems.clear()
-            ugcItems.clear()
-            carouselItems.addAll(data.carouselData?.items ?: emptyList())
-            ugcItems.addAll(data.items)
-            nextPage = data.next
-            showCarousel = carouselItems.isNotEmpty()
-        }.onFailure {
-            logger.fInfo { "load $ugcType data failed: ${it.stackTraceToString()}" }
-            withContext(Dispatchers.Main) {
-                "加载 $ugcType 数据失败: ${it.message}".toast(context)
-            }
-        }
-        hasMore = true
-        updating = false
-    }
-
-    fun reloadAll() {
-        logger.fInfo { "reload all $ugcType data" }
-        scope.launch(Dispatchers.IO) {
-            nextPage = UgcRegionPage()
-            hasMore = true
-            showCarousel = true
-            carouselItems.clear()
-            ugcItems.clear()
-            initUgcRegionData()
-        }
-    }
-
-    suspend fun loadMore() {
-        if (!hasMore && updating) return
-        updating = true
-        runCatching {
-            val data = ugcRepository.getRegionMoreData(ugcType)
-            ugcItems.addAll(data.items)
-            nextPage = data.next
-            hasMore = data.items.isNotEmpty()
-        }.onFailure {
-            logger.fInfo { "load more $ugcType data failed: ${it.stackTraceToString()}" }
-            withContext(Dispatchers.Main) {
-                "加载 $ugcType 更多推荐失败: ${it.message}".toast(context)
-            }
-        }
-        updating = false
-    }
-}
-
-@Composable
-fun rememberUgcScaffoldState(
-    context: Context = LocalContext.current,
-    scope: CoroutineScope = rememberCoroutineScope(),
-    lazyListState: LazyListState = rememberLazyListState(),
-    ugcType: UgcType,
-    ugcRepository: UgcRepository = koinInject()
-): UgcScaffoldState {
-    return remember(
-        context,
-        scope,
-        lazyListState,
-        ugcType,
-        ugcRepository
-    ) {
-        UgcScaffoldState(
-            context = context,
-            scope = scope,
-            lazyListState = lazyListState,
-            ugcType = ugcType,
-            ugcRepository = ugcRepository
-        )
     }
 }
