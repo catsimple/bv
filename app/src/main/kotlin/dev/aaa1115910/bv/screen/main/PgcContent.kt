cfbd42d95a3800df84d60917e358ca923d7ee58a
catsimple <catsimple@qq.com>
2025-03-22 23:56:03 +0800

    Update PgcContent.kt

--- app/src/main/kotlin/dev/aaa1115910/bv/screen/main/PgcContent.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/screen/main/PgcContent.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.focus.focusable
 import androidx.compose.ui.unit.dp
 import dev.aaa1115910.bv.component.PgcTopNavItem
 import dev.aaa1115910.bv.component.TopNav
