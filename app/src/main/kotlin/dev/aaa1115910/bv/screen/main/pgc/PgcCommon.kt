f3ef36d8788de4446ccf54063049ab2c2f30586d
catsimple <catsimple@qq.com>
2025-03-23 01:38:38 +0800

    Update PgcCommon.kt

--- app/src/main/kotlin/dev/aaa1115910/bv/screen/main/pgc/PgcCommon.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/screen/main/pgc/PgcCommon.kt
@@ -25,7 +25,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
@@ -77,6 +83,7 @@ fun PgcScaffold(
     featureButtons: (@Composable () -> Unit)? = null
 ) {
     val carouselFocusRequester = remember { FocusRequester() }
+    var isCarouselFocused by remember { mutableStateOf(false) }
 
     val carouselItems = pgcViewModel.carouselItems
     val pgcFeeds = pgcViewModel.feedItems
@@ -96,7 +103,9 @@ fun PgcScaffold(
                     modifier = Modifier
                         .width(880.dp)
                         .padding(32.dp, 0.dp)
-                        .focusRequester(carouselFocusRequester),
+                        .onFocusChanged { 
+                            isCarouselFocused = it.hasFocus
+                        },
                     data = carouselItems
                 )
             }
