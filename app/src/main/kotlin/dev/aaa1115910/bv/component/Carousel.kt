543145b63ee1ef166e7dc8c011ccfcbe748d76c1
catsimple <catsimple@qq.com>
2025-03-23 12:48:25 +0800

    Update Carousel.kt

--- app/src/main/kotlin/dev/aaa1115910/bv/component/Carousel.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/component/Carousel.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.focusable
+import androidx.compose.foundation.focusGroup
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
