26e1fdb579c3674bd86d798bc7e39d4603ea6711
catsimple <catsimple@qq.com>
2025-03-23 14:09:03 +0800

    Update Carousel.kt

--- app/src/main/kotlin/dev/aaa1115910/bv/component/Carousel.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/component/Carousel.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.focus.focusProperties
 import androidx.tv.material3.Carousel
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.MaterialTheme
