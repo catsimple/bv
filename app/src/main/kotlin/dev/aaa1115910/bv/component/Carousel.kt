70180171e7515f44a35489db0f86cd30a120f0a7
DESKTOP-ORCV9JG\Administrator <catsimple@qq.com>
2025-03-24 15:40:44 +0800

    no message

--- app/src/main/kotlin/dev/aaa1115910/bv/component/Carousel.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/component/Carousel.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.tv.material3.Carousel
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.MaterialTheme
